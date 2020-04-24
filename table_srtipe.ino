309405b100e3f8de79a1dc20611e9f360501ce94
Qui-Gon-Jin <plokun2000@gmail.com>
2020-04-24 19:41:23 +0300

    Update table_srtipe.ino

--- table_srtipe.ino
+++ table_srtipe.ino
@@ -4,11 +4,11 @@
 #define STRIPE_PIN 6
 //
 CRGB leds[NUM_LEDS];
-byte counter = 0;
+byte previous_led[NUM_LEDS];
 const int button_pin = 4;
 int color_diff;
 int button_state = 0;
-byte effect_counter = 0; 
+byte effect_counter = 0, counter = 0, color = 0; 
 boolean pulse_direction = true, button_flag = false, button;
 unsigned long last_press;
 //
@@ -33,36 +33,57 @@ void loop() {
 
 	switch(effect_counter){
 		case 0:
-		//rainbow
+				//cyberpanky boi
+		//pink 250, blue 180, green 150
+		color_diff = (200 - 170)/NUM_LEDS;
+		if(previous_led[0] == 0)
+		{
 			for (int i = 0; i < NUM_LEDS; i++ ){
-				leds[i] = CHSV(counter + i * 5, 255, 255);
+				if (i == 0){
+					color = random(150, 250);
+					leds[i] = CHSV(color , 255, 255);
+					previous_led[i] = color;
+				}
+				else{
+					color = random(previous_led[i-1] - 15, previous_led[i-1] + 15);
+					leds[i] = CHSV(color , 255, 255);
+					previous_led[i] = color;
+				}
 			}
+		}
+		else{
+			for (int i = 0; i < NUM_LEDS; i++ ){
+				color = random(previous_led[i] - 10, previous_led[i] + 10);
+				if (color <= 150)
+					color = 250;
+				if (color >= 250)
+					color = 150;
+
+				leds[i] = CHSV(color, 255, 255);
+				previous_led[i] = leds[i];
+			}
+
+		}
 		break;
 		case 1:
 		//sun
-			for (int i = 0; i < NUM_LEDS; i++ ){
+			for (int i = 0; i < NUM_LEDS; i++ )
 				leds[i] = DirectSunlight;
-			}
 		break;
 		case 2:
 		//light violet and blue
-			for (int i = 0; i < NUM_LEDS; i++ ){
+			for (int i = 0; i < NUM_LEDS; i++ )
 				leds[i] = CRGB::BlueViolet;
-			}
 		break;
 		case 3:
-		//cyberpanky boi
-		//pink 250, blue 180, green 150
-		color_diff = (200 - 170)/NUM_LEDS;
-			for (int i = 0; i < NUM_LEDS; i++ ){
-				leds[i] = CHSV(170 + color_diff * i, 255, 255);
-			}
+		//rainbow
+			for (int i = 0; i < NUM_LEDS; i++ )
+				leds[i] = CHSV(counter + i * 5, 255, 255);
 		break;
 		case 4:
 		//void
-			for (int i = 0; i < NUM_LEDS; i++ ){
+			for (int i = 0; i < NUM_LEDS; i++ )
 				leds[i] = CHSV(0,0,0);
-			}
 		break;
 	}
 	//pulsing();
@@ -71,6 +92,7 @@ void loop() {
 	FastLED.show();
 	delay(25);
 }
+
 void pulsing(){
 	counter++;
 	if (pulse_direction == true)
@@ -113,7 +135,7 @@ void pulsing_high(){
 	else
 	{
 		counter--;
-		if (counter == 150)
+		if (counter == 100)
 			pulse_direction = true;
 	}
 	FastLED.setBrightness(counter);
